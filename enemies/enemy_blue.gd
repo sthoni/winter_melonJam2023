119ced3aef26d87e6416a85b76db2d2763ea5bdd
sthon <sthon@hotmail.de>
2023-12-29 16:45:28 +0100

    Spawner funktioniert grob.

---
+++ enemies/enemy_blue.gd
@@ -0,0 +1,11 @@
+extends "res://enemies/enemy.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	color = EnemyColors.BLUE
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
