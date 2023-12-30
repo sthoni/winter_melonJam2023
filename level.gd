97155415e54ee4ada1cee9b6fdbe0c82cebab3ed
Sebastian Thon <sthon@hotmail.de>
2023-12-30 21:38:39 +0100

    Unendlich viele Level.

--- level.gd
+++ level.gd
@@ -1,12 +1,5 @@
 extends Node2D
 
-
-@onready var enemies_in_level = [
-	[Global.EnemyType.BLUE, Global.EnemyType.BLUE],
-	[Global.EnemyType.GREEN, Global.EnemyType.GREEN],
-	[Global.EnemyType.GREEN, Global.EnemyType.GREEN, Global.EnemyType.GREEN],
-	[Global.EnemyType.GREEN, Global.EnemyType.GREEN, Global.EnemyType.GREEN, Global.EnemyType.BLUE, Global.EnemyType.BLUE]
-	]
 @onready var spawners: Array[Node] = get_tree().get_nodes_in_group("spawner")
 @onready var active_level := 0
 @onready var timer:float = 0
@@ -32,7 +25,8 @@ func level_up():
 
 
 func spawn_enemies(level):
-	for enemy in enemies_in_level[active_level]:
+	for enemy in range(active_level+1):
+		var enemy_type = randi() % Global.EnemyType.size()
 		var new_enemy = spawners[randi() % spawners.size()].create_enemy(enemy)
 		get_parent().add_child.call_deferred(new_enemy)
 
