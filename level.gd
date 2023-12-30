c45e64e598d3f137abfd604bdea6a3a75a80540f
Armin <armin@arminlanger.com>
2023-12-30 19:36:04 +0100

    added 'level up' themes

--- level.gd
+++ level.gd
@@ -3,14 +3,25 @@ extends Node2D
 
 @onready var enemies_level_1 = [Global.EnemyType.BLUE,Global.EnemyType.GREEN, Global.EnemyType.GREEN, Global.EnemyType.BLUE]
 @onready var spawners: Array[Node] = get_tree().get_nodes_in_group("spawner")
+@onready var timer:float = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for enemy in enemies_level_1:
 		var new_enemy = spawners[randi() % spawners.size()].create_enemy(enemy)
 		get_parent().add_child.call_deferred(new_enemy)
+	Global.kill.connect(level_up)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if timer > 0:
+		timer -= delta
+		if timer <= 0:
+			$LevelUp.visible = false
+	
+func level_up():
+	$LevelUp.visible = true
+	$LevelUp/Animation.current_animation = "level_up"
+	timer = 1.9
 
--- projectile.gd
+++ projectile.gd
@@ -7,10 +7,10 @@ func _physics_process(delta):
 
 func _on_area_entered(area):
 	if area.is_in_group("mobs"):
-		area.health_component.reduce_health(2)
+		area.health_component.reduce_health(20)
 		queue_free()
 
 func _on_Bullet_body_entered(body):
 	if body.is_in_group("mobs"):
-		body.health_component.reduce_health(2)
+		body.health_component.reduce_health(20)
 	queue_free()
