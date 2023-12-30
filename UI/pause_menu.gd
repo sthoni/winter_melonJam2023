744b8ec10bf25f69d2b3b6bb52321238fc05f773
Armin <armin@arminlanger.com>
2023-12-31 00:22:00 +0100

    'Start New Game'-Button now worksgit pull!

--- UI/pause_menu.gd
+++ UI/pause_menu.gd
@@ -1,7 +1,5 @@
 extends Control
 @onready var main = $"../"
-@onready var main_menu_scene = preload("res://UI/main_menu.tscn")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -16,15 +14,8 @@ func _on_quit_pressed():
 
 func _on_new_game_pressed():
 	Global.global_clear()
-	#main.get_children().clear()
-	#var root_children = get_tree().root.get_children()
-	#for root_child in root_children:
-		#if root_child.get_path() != load("/root/Level") or root_child.get_path() != load("/root/Global"):
-			#root_child.free()
-	get_tree().change_scene_to_packed(main_menu_scene)
-	#get_tree().change_scene_to_packed(load("res://UI/main_menu.tscn"))
-	#.change_scene(load("res://level.tscn"))
-	#get_tree().change_scene(load("res://level.tscn"))
+	main.pauseMenu() #reset engine time scale to 1 
+	get_tree().reload_current_scene()
 
 func _on_resume_pressed():
 	main.pauseMenu()
