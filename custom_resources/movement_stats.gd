c53205620e06508e9da4d6bcf31837f4c8e8720c
sthoni <sthon@hotmail.de>
2024-05-21 18:47:41 +0200

    Theme mit Font erstellt. Außerdem MovementStats verbessert.

--- custom_resources/movement_stats.gd
+++ custom_resources/movement_stats.gd
@@ -2,16 +2,35 @@ extends Resource
 
 class_name MovementStats
 
+signal movement_stats_changed
+
 @export var max_speed: float = 300.0
+@export var start_speed: float = 200.0
 @export var acceleration: float = 1000.0
 @export var friction: float = 500.0
 
+var speed: int : set = set_speed
+
+func set_speed(value : int) -> void:
+	speed = clampi(value, 0, start_speed)
+	movement_stats_changed.emit()
+
 
 func slow_down(amount: int) -> void:
-	if self.max_speed - amount > 0:
-		self.max_speed -= amount
+	if self.speed - amount > 0:
+		self.speed -= amount
 	else:
-		self.max_speed = 10
+		self.speed = 10
+
 
 func speed_up(amount: int) -> void:
-	self.max_speed += amount
+	if self.speed + amount < self.max_speed:
+		self.speed += amount
+	else:
+		self.speed = self.max_speed
+
+
+func create_instance() -> MovementStats:
+	var instance: MovementStats = self.duplicate()
+	instance.speed = start_speed
+	return instance
