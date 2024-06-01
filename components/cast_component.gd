e4d80b344679b40ed32297fdde859eeda475b196
sthoni <sthon@hotmail.de>
2024-06-01 18:46:52 +0200

    Jetzt wird die Richtung auch erst beim Abschuss bestimmt.

--- components/cast_component.gd
+++ components/cast_component.gd
@@ -17,8 +17,6 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	if cast_input.get_cast_intention():
-		var cast_direction := cast_input.get_cast_direction()
-		rotation = cast_direction.angle() + PI / 2
 		request_cast()
 	if cast_input.get_cast_switch() != 0:
 		actor.stats.charm_book.select_next_charm(cast_input.get_cast_switch())
@@ -36,6 +34,8 @@ func prepare_cast() -> void:
 func cast() -> void:
 	if charm_casting:
 		if charm_casting.type == Charm.Type.PROJECTILE:
+			var cast_direction := cast_input.get_cast_direction()
+			rotation = cast_direction.angle() + PI / 2
 			var projectile: Projectile = projectile_scene.instantiate()
 			projectile.caster = actor
 			var level := get_tree().get_first_node_in_group("level") as Level
